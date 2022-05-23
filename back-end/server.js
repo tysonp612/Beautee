76aba4a94301a16aa43f583a016dbaa92b5d2c78
email <phamson061297@gmail.com>
2022-05-23 20:21:46 -0300

    finish backend setup and connect database

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
-
-app.listen(8000, () => {
-  console.log("server listening");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { readdirSync } = require("fs");
+const cors = require("cors");
+dotenv.config();
+app.use(express.json());
+//route
+app.get("/", (req, res) => {
+  res.send("welcome from home");
+});
+// readdirSync("./routes").map((r) =>
+//   app.use("/api", require(`./routes/${r}/${r}.route`))
+// );
+//database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log(err));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
 });
