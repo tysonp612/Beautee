d16d5068e2177bb1492d407f76532f1f89e1691c
email <phamson061297@gmail.com>
2022-05-23 23:09:46 -0300

    adding helper to generate token, and mailer to send mail, finish register technician

--- back-end/controllers/technician.controller.js
+++ back-end/controllers/technician.controller.js
@@ -1,7 +1,8 @@
 const Technician = require("./../models/technician.model");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const { validateEmail } = require("./../helper/validation");
+const { generateToken } = require("./../helper/tokens");
+const { sendVerifiedEmail } = require("./../helper/mailer");
 exports.technicianRegister = async (req, res) => {
   try {
     const {
@@ -23,6 +24,24 @@ exports.technicianRegister = async (req, res) => {
       username,
       color,
       password: cryptedPassword,
+    }).save();
+    const emailVerificationToken = generateToken(
+      { id: technician._id.toString() },
+      "30m"
+    );
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerifiedEmail(technician.email, technician.first_name, url);
+    res.status(200).json({
+      id: technician._id,
+      username: technician.username,
+      picture: technician.picture,
+      first_name: technician.first_name,
+      last_name: technician.last_name,
+      token,
+      verified: technician.verified,
+      message: "Register Success, please activate your email to start",
     });
-  } catch (err) {}
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
