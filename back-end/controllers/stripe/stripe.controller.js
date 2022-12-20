ff3746991d50fe2de13060ed60b680defb202b8b
email <phamson061297@gmail.com>
2022-12-20 18:46:55 -0400

    finish implementing stripe for payment

--- back-end/controllers/stripe/stripe.controller.js
+++ back-end/controllers/stripe/stripe.controller.js
@@ -2,16 +2,15 @@ const Booking = require("./../../models/bookings.model");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 exports.createPaymentIntent = async (req, res) => {
   //1 find Booking
-  console.log("HAHAHAH");
-  res.status(200).json("hehehe");
-  //   const booking = await Booking.findOne({ id: req.data._id });
-  //   //2 get total amount
-  //   let totalAmountPay = booking.price.actualPrice;
-  //   const paymentIntent = await stripe.paymentIntents.create({
-  //     amount: totalAmountPay * 100,
-  //     currency: "usd",
-  //   });
-  //   res.send({
-  //     clientSercret: paymentIntent.client_secret,
-  //   });
+  const booking = await Booking.findById(req.body.bookingId);
+  //2 get total amount
+  let totalAmountPay = booking.price.actualPrice;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmountPay * 100,
+    currency: "usd",
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 };
