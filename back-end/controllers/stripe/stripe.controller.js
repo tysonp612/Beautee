565aab8fd2693f066376d9f2de07a41e9831cede
email <phamson061297@gmail.com>
2022-12-22 11:22:33 -0400

    finishing payment with stripe, and fixing error of showing wrong total payment

--- back-end/controllers/stripe/stripe.controller.js
+++ back-end/controllers/stripe/stripe.controller.js
@@ -1,8 +1,13 @@
 const Booking = require("./../../models/bookings.model");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 exports.createPaymentIntent = async (req, res) => {
-  console.log(req);
-  const totalAmountPay = req.body.totalPayment;
+  //1 find Booking
+  console.log(req.body);
+  const booking = await Booking.findOne({ _id: req.body.bookingId });
+  console.log(booking);
+  //2 get total amount
+  let totalAmountPay = booking.totalPayment;
+  console.log(totalAmountPay);
   const paymentIntent = await stripe.paymentIntents.create({
     amount: totalAmountPay * 100,
     currency: "usd",
